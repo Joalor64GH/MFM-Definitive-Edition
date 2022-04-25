570e191122742dc5cb4ad0fa3f3cfaf92bfad57e
Joalor64 <rhgarcon@gmail.com>
2022-04-25 07:25:15 -0400

    quick fps thing

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -87,7 +87,7 @@ class FPS extends TextField
 			
 			#if openfl
 			memoryMegas = Math.abs(FlxMath.roundDecimal(System.totalMemory / 1000000, 1));
-			text += "\nMemory Used: " + memoryMegas + " MB";
+			text += "\nMemory Using: " + memoryMegas + " MB";
 			#end
 
 			textColor = 0xFFFFFFFF;
